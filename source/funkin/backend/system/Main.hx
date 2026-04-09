d3230ee951f62a14b160182fd958af575f709000
ArkoseLabsOfficial <28umutmehmet28@gmail.com>
2026-04-09 14:32:22 +0000

    I hate using androidtools but currently i can add for making the port

--- source/funkin/backend/system/Main.hx
+++ source/funkin/backend/system/Main.hx
@@ -24,8 +24,8 @@ import openfl.utils.AssetLibrary;
 import sys.FileSystem;
 import sys.io.File;
 #if android
-import android.content.Context;
-import android.os.Build;
+import androidtools.content.Context;
+import androidtools.os.Build;
 #end
 
 class Main extends Sprite
